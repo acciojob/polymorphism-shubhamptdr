310350f5b6663ea55cf7502fb7b10978b08bef4c
Shubham Patidar <skpatidar222@gmail.com>
2023-01-13 11:00:29 +0530

    solution 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,15 +7,15 @@ public class Main {
         p.product(2,4,1);
         p.product(2.0,4.0);
     }
-}
-class Product {
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
+    public static class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
     }
 }
